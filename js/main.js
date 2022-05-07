d26e29e303f08458ab215ae573de3500fbd3c59b
Fleisar <me@fle.su>
2022-05-07 13:06:21 +0700

    fix scrollTop

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 const windowJQ = $(window);
 windowJQ.on('scroll', () => {
-	const scrolled = windowJQ.scrollTop;
+	const scrolled = windowJQ.scrollTop();
 	$('.head').css({
 		background: scrolled > 50 ? '#222c' : '#2110',
 	});
