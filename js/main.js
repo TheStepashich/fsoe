547747e261a02f551ee94324a6622a02ff8dac03
Fleisar <me@fle.su>
2022-05-07 14:50:46 +0700

    full refactor

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const windowJQ = $(window);
 windowJQ.on('scroll', () => {
 	const scrolled = windowJQ.scrollTop();
-	$('.head').css({
-		background: scrolled > 50 ? '#222c' : '#2110',
+	$('header').css({
+		'background-color': scrolled > 40 ? '#222c' : '#2220',
 	});
 });
